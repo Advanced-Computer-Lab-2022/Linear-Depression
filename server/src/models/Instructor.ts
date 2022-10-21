96027ac1c7125512e791f387d9c87e0c903cf630
Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>
2022-10-21 22:16:14 +0200

    Update server/src/models/Instructor.ts
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>

--- server/src/models/Instructor.ts
+++ server/src/models/Instructor.ts
@@ -1,6 +1,5 @@
 import mongoose, { Document, Schema } from "mongoose";
 import User, { IUser } from "./User";
-import uniqueValidator from "mongoose-unique-validator";
 const options = { discriminatorKey: "kind" };
 
 export interface IInstructor extends IUser {
