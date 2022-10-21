de8c4c8c2b90fe0b6b20d5b710d8f90fe24b2bf3
Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>
2022-10-21 23:39:13 +0200

    Add User model (#14)
    * Add User model
    
    * Update User Schema
    
    * Add trim and lowercase to email and userNAme fields
    
    * Delete package-lock.json
    
    * Add Instructor Model. (#16)
    
    * Refactor
    
    remove unused imports
    add link for testing email regex
    
    * Update server/src/models/User.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/Instructor.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/Instructor.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/User.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Add Course Model (#17)
    
    * Add Couse Model
    
    * Update server/src/models/Course.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/Course.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/Course.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/Course.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Update server/src/models/Course.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Refactor Course Model
    
    * Refactor Course Model
    
    * Refactor Course Model
    
    * Add rating model and refactor course model
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    
    * Refactor Instructor
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>
    Co-authored-by: Abdulaziz Hassan <abdulazizhza@gmail.com>

---
+++ server/src/models/Instructor.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Schema } from "mongoose";
+import User, { IUser } from "./User";
+const options = { discriminatorKey: "kind" };
+
+export interface IInstructor extends IUser {
+  courses: Array<mongoose.Types.ObjectId>; // Array of course IDs
+}
+
+// inherit from IUserModel
+export interface IInstructorModel extends IInstructor, Document {}
+
+const Instructor = User.discriminator(
+  "Instructor",
+  new Schema(
+    {
+      courses: [{ type: mongoose.Types.ObjectId, ref: "Course" }]
+    },
+    options
+  )
+);
+
+export default Instructor;
---
+++ server/src/models/User.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  passwordHash: string;
+}
+export interface IUserModel extends IUser, Document {}
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/\S+@\S+\.\S+/, "is invalid"], //regexr.com/70m6a
+    trim: true,
+    lowercase: true
+  },
+  userName: { type: String, required: true, unique: true, trim: true, lowercase: true },
+  passwordHash: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator, { message: "is already taken." });
+export default mongoose.model<IUserModel>("User", userSchema);
