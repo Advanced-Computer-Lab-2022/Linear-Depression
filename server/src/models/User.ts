e45521648998978a18078b173124fd30c1bb27af
ShimaaBetah <shimaabetah1911@gmail.com>
2022-10-21 19:39:27 +0200

    Add User model

---
+++ server/src/models/User.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+}
+export interface IUserModel extends IUser, Document {}
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+UserSchema.plugin(uniqueValidator, { message: "is already taken." });
+export default mongoose.model<IUserModel>("User", UserSchema);
