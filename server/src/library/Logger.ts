1577128dd893126166ece91a7bfa2322f9505af9
Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>
2022-10-21 16:53:20 +0200

    Add initial Structure files (#1)
    * Add initial Structure files
    
    * Update server/src/server.ts
    
    * Update server/package.json
    
    * Update server/src/server.ts
    
    * Update server/src/server.ts
    
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>

---
+++ server/src/library/Logger.ts
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+
+export default class Logger {
+  public static log = (args: any) => {
+    this.info(args);
+  };
+  public static info = (args: any) => {
+    console.log(
+      chalk.blue(`[${new Date().toLocaleString()}] [INFO]`),
+      typeof args === "string"
+        ? chalk.blueBright(args)
+        : chalk.blueBright(JSON.stringify(args))
+    );
+  };
+  public static warn = (args: any) => {
+    console.log(
+      chalk.blue(`[${new Date().toLocaleString()}] [Warning]`),
+      typeof args === "string"
+        ? chalk.yellowBright(args)
+        : chalk.yellowBright(JSON.stringify(args))
+    );
+  };
+  public static error = (args: any) => {
+    console.log(
+      chalk.red(`[${new Date().toLocaleString()}] [ERROR]`),
+      typeof args === "string"
+        ? chalk.redBright(args)
+        : chalk.redBright(JSON.stringify(args))
+    );
+  };
+}
---
+++ server/src/server.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import Logger from "./library/Logger";
+import { config } from "./config/config";
+const app = express();
+
+/*connect to MongoDB*/
+mongoose
+  .connect(config.mongo.config, {
+    retryWrites: true,
+    w: "majority",
+  })
+  .then(() => {
+    Logger.log("Connected to MongoDB");
+    startServer();
+  })
+  .catch((err) => {
+    Logger.log(err);
+  });
+
+/*create server*/
+const startServer = () => {
+  app.use((req, res, next) => {
+    /* log the request */
+    Logger.info(
+      `Incoming -> Method [${req.method}] - URL [${req.url}] - IP [${req.socket.remoteAddress}]`
+    );
+
+    res.on("finish", () => {
+      /* log the response */
+      Logger.info(
+        `Outgoing -> Status [${res.statusCode}] - Method [${req.method}] - URL [${req.url}] - IP [${req.socket.remoteAddress}]`
+      );
+    });
+
+    next();
+  });
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  /* Routers*/
+  
+  /*Health Check*/
+  app.get("/ping", (req, res) => {
+    return res.status(200).json({message:"pong"});
+  });
+
+  /*404*/
+  app.use((req, res) => {
+    const error = new Error("Not Found");
+    Logger.error(error);
+    return res.status(404).json({message: error.message});
+  });
+
+ app.listen(config.server.port, () => {
+    Logger.log(`Server started at port ${config.server.port}`);
+  });
+};
