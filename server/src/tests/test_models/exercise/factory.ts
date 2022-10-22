ba6e256d8a6097d68180a60ae9df08f92882091f
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2022-10-22 22:36:37 +0200

    Fix unresolved comments

--- server/src/tests/test_models/exercise/factory.ts
+++ server/src/tests/test_models/exercise/factory.ts
@@ -1,41 +1,29 @@
 import { faker } from "@faker-js/faker";
+import { IMCQuestion } from "../../../models/Course";
 import { IExercise } from "../../../models/Exercise";
+
+const createQuestions = (randomLength: Number): IMCQuestion[] => {
+    const questions: IMCQuestion[] = [];
+    for (let i = 0; i < randomLength; i++) {
+        questions.push({
+            question: faker.lorem.sentence(),
+            choices: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
+            answerIndex: faker.datatype.number({
+                min: 0,
+                max: 3
+            })
+        });
+    }
+    return questions;
+};
 
 export const exerciseFactory = (): IExercise => {
     return {
-        questions: [
-            {
-                question: faker.lorem.sentence(),
-                choices: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
-                answerIndex: faker.datatype.number({
-                    min: 0,
-                    max: 3
-                })
-            },
-            {
-                question: faker.lorem.sentence(),
-                choices: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
-                answerIndex: faker.datatype.number({
-                    min: 0,
-                    max: 3
-                })
-            },
-            {
-                question: faker.lorem.sentence(),
-                choices: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
-                answerIndex: faker.datatype.number({
-                    min: 0,
-                    max: 3
-                })
-            },
-            {
-                question: faker.lorem.sentence(),
-                choices: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
-                answerIndex: faker.datatype.number({
-                    min: 0,
-                    max: 3
-                })
-            }
-        ]
+        questions: createQuestions(
+            faker.datatype.number({
+                min: 1,
+                max: 5
+            })
+        )
     };
 };
