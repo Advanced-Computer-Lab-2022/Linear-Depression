bc655851d406f037c9b18cf0d9ea73ff73b2d5a0
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-22 19:12:07 +0200

    feat: setup API rules

--- server/src/server.ts
+++ server/src/server.ts
@@ -36,6 +36,19 @@ const startServer = () => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
+    /** Rules of our API */
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+        if (req.method == "OPTIONS") {
+            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
     /* Routers*/
 
     /*Health Check*/
