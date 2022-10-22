68c33c0256c0c89975825c4c189458d7d52dde93
Abdulaziz Hassan <abdulazizhza@gmail.com>
2022-10-22 22:45:36 +0200

    Add course unit tests (#28)
    * Add course factory
    
    * Add course unit test
    
    * Add factory and unit tests for course and exercise
    
    * Add rating factory and unit test
    
    * Fix unresolved comments
    
    * Add course factory
    
    * Add course unit test
    
    * Add factory and unit tests for course and exercise
    
    * Add rating factory and unit test
    
    * Fix conflicts
    
    * Fix unresolved comments

--- server/src/tests/test_models/course/factory.ts
+++ server/src/tests/test_models/course/factory.ts
@@ -1,6 +1,22 @@
 import { faker } from "@faker-js/faker";
-import { ICourse } from "../../../models/Course";
+import { ICourse, ILesson } from "../../../models/Course";
 import mongoose from "mongoose";
+
+const createExercises = (randomLength: Number): ILesson[] => {
+    const exercises: ILesson[] = [];
+    for (let i = 0; i < randomLength; i++) {
+        exercises.push({
+            title: faker.lorem.words(),
+            exercises: [new mongoose.Types.ObjectId(faker.database.mongodbObjectId())],
+            totalHours: faker.datatype.number(),
+            video: {
+                videoLink: faker.internet.url(),
+                description: faker.lorem.paragraph()
+            }
+        });
+    }
+    return exercises;
+};
 
 export function courseFactory(): ICourse {
     return {
@@ -18,16 +34,11 @@ export function courseFactory(): ICourse {
         ratings: [new mongoose.Types.ObjectId(faker.database.mongodbObjectId())],
         totalHours: faker.datatype.number(),
         preview: faker.internet.url(),
-        lessons: [
-            {
-                title: faker.lorem.words(),
-                exercises: [new mongoose.Types.ObjectId(faker.database.mongodbObjectId())],
-                totalHours: faker.datatype.number(),
-                video: {
-                    videoLink: faker.internet.url(),
-                    description: faker.lorem.paragraph()
-                }
-            }
-        ]
+        lessons: createExercises(
+            faker.datatype.number({
+                min: 1,
+                max: 5
+            })
+        )
     };
 }
