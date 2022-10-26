3f916307041dc633db682654a3ada62de0bfaa2d
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-26 09:19:15 +0200

    fix: remove default css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import CorporateTrainee from "./pages/CorporateTrainee";
 import IndividualTrainee from "./pages/IndividualTrainee";
