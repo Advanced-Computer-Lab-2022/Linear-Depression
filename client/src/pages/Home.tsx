3393aa41c83b3437ba9c5e14a1fe71375f77984a
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-26 09:16:24 +0200

    feat: fetch courses data from api

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,24 +1,65 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import CoursesList from "../components/CoursesList";
 import Navbar from "../components/Navbar";
-
-const Temp = styled.div`
-    color: red;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
+import axios from "axios";
 
 const Home: React.FC = () => {
+    const [courses, setCourses] = useState({
+        data: [],
+        loading: true,
+        error: null
+    });
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/courses")
+            .then((res) => {
+                console.log(res.data.courses);
+                setCourses({
+                    data: res.data.courses,
+                    loading: false,
+                    error: null
+                });
+            })
+            .catch((err) => {
+                setCourses({
+                    data: [],
+                    loading: false,
+                    error: err
+                });
+            });
+    }, []);
     return (
-        <>
+        <div>
             <Navbar />
-            <Temp>
-                <h1>Home</h1>
-            </Temp>
-        </>
+            <CoursesList courses={courses.data} />
+        </div>
     );
 };
 
 export default Home;
+
+// [
+//     {
+//         id: "1",
+//         title: "100 Days of Code: The Complete Python Pro Bootcamp for 2022",
+//         description:
+//             "Learn Python like a Professional! Start from the basics and go all the way to creating your own applications and games!",
+//         instructor: "Dr. Angela Yu",
+//         rating: 4.3,
+//         duration: 10,
+//         price: 12.99,
+//         currency: "$"
+//     },
+//     {
+//         id: "2",
+//         title: "100 Days of Code: The Complete Python Pro Bootcamp for 2022",
+//         description:
+//             "Learn Python like a Professional! Start from the basics and go all the way to creating your own applications and games!",
+//         instructor: "Dr. Angela Yu",
+//         rating: 4.3,
+//         duration: 10,
+//         price: 12.99,
+//         currency: "$"
+//     }
+// ];
