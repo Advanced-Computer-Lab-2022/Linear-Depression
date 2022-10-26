212ffc43d2da1ae5fec60096cc763f3f141b9bc8
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-10-26 14:19:15 +0200

    Add course card - courses list (#50)
    * feat: add CourseCard component
    
    * feat: add CoursesList component
    
    * feat: add types
    
    * style: fix navbar style
    
    * refactor: extract course props interface
    
    * feat: fetch courses data from api
    
    * refactor: remove commented props
    
    * fix: remove default css

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,23 +1,39 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import CoursesList from "../components/CoursesList";
 import Navbar from "../components/Navbar";
-
-const Temp = styled.div`
-    color: red;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
+import axios from "axios";
 
 const Home: React.FC = () => {
+    const [courses, setCourses] = useState({
+        data: [],
+        loading: true,
+        error: null
+    });
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/courses")
+            .then((res) => {
+                console.log(res.data.courses);
+                setCourses({
+                    data: res.data.courses,
+                    loading: false,
+                    error: null
+                });
+            })
+            .catch((err) => {
+                setCourses({
+                    data: [],
+                    loading: false,
+                    error: err
+                });
+            });
+    }, []);
     return (
-        <>
+        <div>
             <Navbar />
-            <Temp>
-                <h1>Home</h1>
-            </Temp>
-        </>
+            <CoursesList courses={courses.data} />
+        </div>
     );
 };
 
