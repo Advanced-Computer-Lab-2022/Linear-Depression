0a38bb56360cf52e73631e84907a688004375425
Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>
2022-10-28 18:17:26 +0200

    Add country select component (#56)
    * Add Corporate and Individual Trainee models
    
    create class for User Schema to inherit from
    create class for traineeschema which extends UserSchemaClass
    create 2 models for individual and corporate trainee that use discriminator on User model
    
    * api's for currency.
    
    * refactor.
    
    * fix bug.
    
    * Add Country Select component
    
    * Call setCountry api
    
    * cookie.
    
    * forgot to push this.
    
    * Add Country Context
    
    * Refactor
    
    Co-authored-by: aboueleyes <ibrahimabouelanin@gmail.com>
    Co-authored-by: Ibrahim Abou Elenein <35760882+aboueleyes@users.noreply.github.com>

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,12 +5,13 @@ dotenv.config();
 const MONGO_URL = process.env.MONGO_URL || "";
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
-
+const FRONT_END_URL = process.env.FRONT_END_URL || "http://localhost:3001";
 export const config = {
     mongo: {
         config: MONGO_URL
     },
     server: {
         port: SERVER_PORT
-    }
+    },
+    FRONT_END_URL: FRONT_END_URL
 };
