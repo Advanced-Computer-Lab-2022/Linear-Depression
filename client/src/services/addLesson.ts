3d7152fb85eb1698b5b3cde19d32f3f4a0954707
AhmedNasserG <ahmednasser217217@gmail.com>
2022-11-19 23:59:01 +0200

    refactor: AddLessonForm component

---
+++ client/src/services/addLesson.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { config } from "../config/config";
+
+const addLesson = (courseId: string, lesson: {}) => {
+    const ADD_LESSON_URL = `${config.API_URL}/courses/${courseId}/add-lesson`;
+    return new Promise((resolve, reject) => {
+        axios
+            .post(ADD_LESSON_URL, lesson, { withCredentials: true })
+            .then((res) => {
+                resolve(res.data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+};
+
+export default addLesson;
