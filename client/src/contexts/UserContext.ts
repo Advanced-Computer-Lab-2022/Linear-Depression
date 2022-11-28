af1b7441905d97ecd1ba09d71f40aa6539e48b5a
ShimaaBetah <shimaabetah1911@gmail.com>
2022-11-28 14:34:03 +0200

    Delete User ID from the context

--- client/src/contexts/UserContext.ts
+++ client/src/contexts/UserContext.ts
@@ -3,9 +3,7 @@ import { createContext, Dispatch, SetStateAction } from "react";
 import { User } from "@internals/types";
 
 const UserContext = createContext({
-    userId: "",
     userType: User.GUEST,
-    setUserId: {} as Dispatch<SetStateAction<string>>,
     setUserType: {} as Dispatch<SetStateAction<User>>
 });
 
