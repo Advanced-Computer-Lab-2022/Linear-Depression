c9b3b8eff02fafa92e9c3a469be796c0cb623bd1
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2022-11-30 00:17:25 +0200

    Add Exercise components
    Co-authored-by: ShimaaBetah <shimaabetah1911@gmail.com>
    Co-authored-by: Ahmed Nasser <ahmednasser217217@gmail.com>

---
+++ client/src/components/exercise/SubmitButton.ts
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import styled from "styled-components";
+
+const SubmitButton = styled(Button)`
+    margin-top: 1rem;
+    margin-left: auto;
+    margin-right: 1rem;
+    background-color: #3f51b5 !important;
+`;
+
+export default SubmitButton;
