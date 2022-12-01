a8e274034aea00367f7007fe6887807ee56d6c77
aboueleyes <ibrahimabouelanin@gmail.com>
2022-12-01 22:03:27 +0200

    add populate test db.

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,6 +20,8 @@ import ExerciseRouter from "./routes/Exercise";
 import AuthRouter from "./routes/Auth";
 import MeRouter from "./routes/Me";
 
+import { populateTestDb } from "./utils/populateTestDb";
+
 const cors = require("cors");
 import * as path from "path";
 
@@ -108,4 +110,8 @@ app.use((req, res) => {
 // getCurrencyRatesTask.start();
 /* --- End Routes --- */
 
+//NOTE: Use this to populate the database with test data
+// for (let i = 0; i < 2; i++) {
+//     populateTestDb();
+// }
 export default app;
