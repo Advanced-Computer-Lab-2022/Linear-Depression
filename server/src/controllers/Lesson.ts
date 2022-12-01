d0b3ff2fb66057504dfe815b052ce38f4f0809da
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2022-12-01 15:08:00 +0200

    Update Course API by extracting Lesson API in a seaparate controller
    Co-authored-by: ShimaaBetah <shimaabetah1911@gmail.com>

---
+++ server/src/controllers/Lesson.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Course from "../models/Course";
+import Lesson from "../models/Lesson";
+
+const createLesson = async (req: Request, res: Response, _next: NextFunction) => {
+    const courseId = req.params.courseId;
+    const lesson = new Lesson({
+        ...req.body
+    });
+    lesson
+        .save()
+        .then((lesson) => {
+            Course.findByIdAndUpdate(courseId, { $push: { lessons: lesson._id } }).then(() => {
+                res.status(StatusCodes.CREATED).json({ lesson });
+            });
+        })
+        .catch((error) => res.status(StatusCodes.BAD_REQUEST).json({ error: error.message }));
+};
+
+const updateLesson = async (req: Request, res: Response, _next: NextFunction) => {
+    const lessonId = req.params.lessonId;
+
+    return Lesson.findById(lessonId)
+        .then((lesson) => {
+            if (lesson) {
+                lesson.set(req.body);
+
+                return lesson
+                    .save()
+                    .then((lesson) => res.status(StatusCodes.OK).json({ lesson }))
+                    .catch((error) => res.status(StatusCodes.BAD_REQUEST).json({ error }));
+            } else {
+                return res.status(StatusCodes.NOT_FOUND).json({ error: "Lesson not found" });
+            }
+        })
+        .catch((error) => res.status(StatusCodes.BAD_REQUEST).json({ error: error.message }));
+};
+
+export default {
+    createLesson,
+    updateLesson
+};
