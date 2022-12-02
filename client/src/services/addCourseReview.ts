22b9a3c50c3a8dee7b06b0d6465b13ebe97ee742
Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>
2022-12-02 15:25:45 +0200

    Update client/src/services/addCourseReview.ts

--- client/src/services/addCourseReview.ts
+++ client/src/services/addCourseReview.ts
@@ -5,7 +5,6 @@ import { config } from "@internals/config";
 const addCourseReview = (courseId: string, rating: number, comment: string) => {
     const traineeId = "63638d2dfa52a234ba846b39"; //TODO: remove when cookie is implemented
     const REVIEW_POST_URL = `${config.API_URL}/courses/${courseId}/ratings`;
-    console.log(courseId);
     return new Promise((resolve, reject) => {
         axios
             .post(
