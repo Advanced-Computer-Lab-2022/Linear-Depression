629c11a31eb71ddf03e6fdc7d06431d922dd09b5
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2022-12-03 01:03:27 +0200

    Add Instructro type
    Co-authored-by: ShimaaBetah <shimaabetah1911@gmail.com>

---
+++ client/src/types/Instructor.ts
@@ -0,0 +1,10 @@
+interface Instructor {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    email: string;
+    biography: string;
+}
+
+export default Instructor;
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -3,8 +3,19 @@ import Course from "./Course";
 import Evaluation from "./Evaluation";
 import Exercise from "./Exercise";
 import FormProps from "./FormProps";
+import Instructor from "./Instructor";
 import Lesson from "./Lesson";
 import Promotion from "./Promotion";
 import User from "./User";
 
-export { type Country, type Course, type FormProps, type Lesson, type Promotion, User, type Exercise, type Evaluation };
+export {
+    type Country,
+    type Course,
+    type FormProps,
+    type Lesson,
+    type Promotion,
+    User,
+    type Exercise,
+    type Evaluation,
+    type Instructor
+};
