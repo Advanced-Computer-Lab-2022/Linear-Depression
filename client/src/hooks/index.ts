c070b0ced101889bcf0ca0211565924cbac7056a
ShimaaBetah <shimaabetah1911@gmail.com>
2022-12-04 16:17:27 +0200

    Add FetchMyReviews hook

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -10,3 +10,4 @@ export { default as useGetUserType } from "./useGetUserType";
 export { default as useFetchProfile } from "./useFetchProfile";
 export { default as useFetchLessonById } from "./useFetchLessonById";
 export { default as useGetInstructorContractStatus } from "./useGetInstructorContractStatus";
+export { default as useFetchMyReviews } from "./useFetchMyReviews";
