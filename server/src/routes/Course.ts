ac153b13756665a8412ca1f99a3426d8f094e8a6
AhmedNasserG <ahmednasser217217@gmail.com>
2022-12-21 22:04:03 +0200

    feat: add authorization to course endpoints

--- server/src/routes/Course.ts
+++ server/src/routes/Course.ts
@@ -3,41 +3,101 @@ import controller from "../controllers/Course";
 import ratingController from "../controllers/CourseRating";
 import exerciseController from "../controllers/Exercise";
 import lessonController from "../controllers/Lesson";
-import authenticated from "../middleware/isAuthenticated";
+import { UserType } from "../enums/UserTypes";
+import isAuthenticated from "../middleware/isAuthenticated";
+import isAuthorized from "../middleware/isAuthorized";
+import isCourseOwner from "../middleware/isCourseOwner";
+import isRatingOwner from "../middleware/isRatingOwner";
 
 const router = express.Router();
 
+// all users
 router.get("/", controller.listCourses);
-router.post("/", authenticated, controller.createCourse);
 router.get("/subjects", controller.listSubjects);
 router.get("/:courseId", controller.readCourse);
-router.put("/:courseId", controller.updateCourse);
-router.delete("/:courseId", controller.deleteCourse);
-
-router.post("/:courseId/lessons", lessonController.createLesson);
-router.get("/:courseId/lessons/:lessonId", lessonController.readLesson);
-router.put("/:courseId/lessons/:lessonId", lessonController.updateLesson);
-
 router.get("/:courseId/ratings", ratingController.listRatings);
-router.post("/:courseId/ratings", authenticated, ratingController.createRating);
 router.get("/:courseId/ratings/:ratingId", ratingController.readRating);
-router.put("/:courseId/ratings/:ratingId", ratingController.updateRating);
-router.delete("/:courseId/ratings/:ratingId", authenticated, ratingController.deleteRating);
 
-router.get("/:courseId/lessons/:lessonId/exercises", exerciseController.listExercises);
-router.get("/:courseId/lessons/:lessonId/exercises/:exerciseId", exerciseController.readExercise);
-router.post("/:courseId/lessons/:lessonId/exercises", exerciseController.createExercise);
-router.put("/:courseId/lessons/:lessonId/exercises/:exerciseId", exerciseController.updateExercise);
-router.delete("/:courseId/lessons/:lessonId/exercises/:exerciseId", exerciseController.deleteExercise);
+// instructor
+router.post("/", isAuthenticated, isAuthorized([UserType.INSTRUCTOR]), controller.createCourse);
 
+// instructor and owner
+router.put("/:courseId", isAuthenticated, isAuthorized([UserType.INSTRUCTOR]), isCourseOwner, controller.updateCourse);
+router.delete(
+    "/:courseId",
+    isAuthenticated,
+    isAuthorized([UserType.INSTRUCTOR]),
+    isCourseOwner,
+    controller.deleteCourse
+);
+router.post(
+    "/:courseId/lessons",
+    isAuthenticated,
+    isAuthorized([UserType.INSTRUCTOR]),
+    isCourseOwner,
+    lessonController.createLesson
+);
+router.put(
+    "/:courseId/lessons/:lessonId",
+    isAuthenticated,
+    isAuthorized([UserType.INSTRUCTOR]),
+    isCourseOwner,
+    lessonController.updateLesson
+);
+router.post(
+    "/:courseId/lessons/:lessonId/exercises",
+    isAuthenticated,
+    isAuthorized([UserType.INSTRUCTOR]),
+    isCourseOwner,
+    exerciseController.createExercise
+);
+router.put(
+    "/:courseId/lessons/:lessonId/exercises/:exerciseId",
+    isAuthenticated,
+    isAuthorized([UserType.INSTRUCTOR]),
+    isCourseOwner,
+    exerciseController.updateExercise
+);
+router.delete(
+    "/:courseId/lessons/:lessonId/exercises/:exerciseId",
+    isAuthenticated,
+    isAuthorized([UserType.INSTRUCTOR]),
+    isCourseOwner,
+    exerciseController.deleteExercise
+);
+
+// trainee and enrolled
+router.get("/:courseId/lessons/:lessonId", lessonController.readLesson);
+router.post("/:courseId/ratings", isAuthenticated, ratingController.createRating);
 router.get(
     "/:courseId/lessons/:lessonId/exercises/:exerciseId/submissions",
-    authenticated,
+    isAuthenticated,
     exerciseController.readSubmission
 );
 router.post(
     "/:courseId/lessons/:lessonId/exercises/:exerciseId/submissions",
-    authenticated,
+    isAuthenticated,
     exerciseController.submitExercise
 );
+
+// trainee and owner
+router.put(
+    "/:courseId/ratings/:ratingId",
+    isAuthenticated,
+    isAuthorized([UserType.CORPORATE_TRAINEE, UserType.INDIVIDUAL_TRAINEE]),
+    isRatingOwner,
+    ratingController.updateRating
+);
+router.delete(
+    "/:courseId/ratings/:ratingId",
+    isAuthenticated,
+    isAuthorized([UserType.CORPORATE_TRAINEE, UserType.INDIVIDUAL_TRAINEE]),
+    isRatingOwner,
+    ratingController.deleteRating
+);
+
+// trainee and enrolled or Instructor and owner
+router.get("/:courseId/lessons/:lessonId/exercises", exerciseController.listExercises);
+router.get("/:courseId/lessons/:lessonId/exercises/:exerciseId", exerciseController.readExercise);
+
 export default router;
