cf88f4c07114b014161ed2f01eefb0d828d373db
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2022-12-23 18:05:59 +0200

    Refactor change filename grammar
    Co-authored-by: Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>

--- client/src/services/note/index.ts
+++ client/src/services/note/index.ts
@@ -1,5 +1,5 @@
 export { default as fetchMyNote } from "./fetchMyNote";
 export { default as addNote } from "./addNote";
 export { default as editNote } from "./editNote";
-export { default as saveToPDF } from "./saveToPDF";
+export { default as saveAsPDF } from "./saveAsPDF";
 export { default as downloadPDF } from "./downloadPDF";
