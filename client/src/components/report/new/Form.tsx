22758a0c40feb62b8e19fe3f6d339a26a97fe8ba
Mohammad Omar <mohammadomartaha17@gmail.com>
2022-12-25 16:37:43 +0200

    feat: add form validations

--- client/src/components/report/new/Form.tsx
+++ client/src/components/report/new/Form.tsx
@@ -1,19 +1,78 @@
-import { FormLabel, Select, Option, TextField, Textarea, Button, Sheet } from "@mui/joy";
+import { Report, CloseRounded } from "@mui/icons-material";
+import {
+    FormLabel,
+    Select,
+    Option,
+    TextField,
+    Textarea,
+    Button,
+    Sheet,
+    Divider,
+    Chip,
+    Alert,
+    IconButton
+} from "@mui/joy";
+import { StatusCodes } from "http-status-codes";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import * as Yup from "yup";
 
-import { HandleReportFormSubmit } from "@internals/handlers";
-import { ReportType } from "@internals/types";
+import { AddReport } from "@internals/api";
+import { HorizontalCourseCard } from "@internals/components";
+import { ReportType, ReportFormProps } from "@internals/types";
+import { validateFormData } from "@internals/utils";
 
 const reportTypes = Object.values(ReportType);
 
+const validationRules = {
+    type: Yup.string().required("Report type is required").oneOf(reportTypes),
+    subject: Yup.string().required("Subject is required").max(45, "Subject must be less than 45 characters"),
+    description: Yup.string()
+        .required("Description is required")
+        .max(500, "Description must be less than 500 characters")
+};
+
 const Form: React.FC = () => {
-    const [selectedType, setSelectedType] = useState(null);
-    const [subject, setSubject] = useState("");
-    const [description, setDescription] = useState("");
+    const [formErrors, setFormErrors] = useState(new Map());
+    const [showAlert, setShowAlert] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const [searchParams] = useSearchParams();
     const navigate = useNavigate();
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setLoading(true);
+
+        setFormErrors(new Map());
+        setShowAlert(false);
+
+        const formData = getFormData(e) as unknown as ReportFormProps;
+        formData.courseId = searchParams.get("course_id") as string;
+
+        validateFormData(formData, validationRules)
+            .then(async (data) => {
+                const validatedData = data as unknown as ReportFormProps;
+
+                await AddReport(validatedData)
+                    .then(() => navigate("/me/reports"))
+                    .catch((err) => {
+                        if (err.response?.status === StatusCodes.BAD_REQUEST) {
+                            setFormErrors(new Map([["courseId", true]]));
+                        }
+
+                        setShowAlert(true);
+                    })
+                    .finally(() => setLoading(false));
+            })
+            .catch((errors) => setFormErrors(errors))
+            .finally(() => setLoading(false));
+    };
+
+    const getFormData = (event: React.FormEvent<HTMLFormElement>) => {
+        return Object.fromEntries(new FormData(event.currentTarget).entries());
+    };
 
     return (
         <Sheet
@@ -24,75 +83,130 @@ const Form: React.FC = () => {
                 display: "flex",
                 flexDirection: "column",
                 borderRadius: "sm",
-                boxShadow: "0 0 0 0px rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)"
+                boxShadow: "0 2px 8px 0 rgba(0, 0, 0, 0.1)"
             }}
         >
-            <FormLabel htmlFor="select" id="select-label" required>
-                Issue Type
-            </FormLabel>
-            <Select
-                placeholder="Choose one…"
-                size="lg"
-                variant="soft"
-                slotProps={{
-                    button: {
-                        id: "select",
-                        "aria-labelledby": "select-label"
-                    }
+            <HorizontalCourseCard />
+
+            <Divider sx={{ my: 1 }}>
+                <Chip variant="soft" color="neutral">
+                    Report Form
+                </Chip>
+            </Divider>
+
+            <Sheet
+                component="form"
+                onSubmit={(e) => {
+                    handleFormSubmit(e);
                 }}
-                onChange={(_e, value) => setSelectedType(value as ReportType)}
+                sx={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column"
+                }}
             >
-                {reportTypes.map((type) => (
-                    <Option key={reportTypes.indexOf(type)} value={type}>
-                        {type}
-                    </Option>
-                ))}
-            </Select>
+                <FormLabel htmlFor="select" id="select-label" required>
+                    Report Type
+                </FormLabel>
+                <Select
+                    name="type"
+                    defaultValue={reportTypes[0]}
+                    size="lg"
+                    variant="soft"
+                    slotProps={{
+                        button: {
+                            id: "select",
+                            "aria-labelledby": "select-label"
+                        }
+                    }}
+                    sx={{ width: "100%" }}
+                >
+                    {reportTypes.map((type) => (
+                        <Option key={reportTypes.indexOf(type)} value={type}>
+                            {type}
+                        </Option>
+                    ))}
+                </Select>
 
-            <FormLabel htmlFor="subject" id="subject-label" sx={{ mt: 2 }} required>
-                Subject
-            </FormLabel>
-            <TextField
-                placeholder="Subject"
-                size="lg"
-                variant="soft"
-                slotProps={{
-                    input: {
-                        id: "subject",
-                        "aria-labelledby": "subject-label"
-                    }
-                }}
-                required
-                onChange={(e) => setSubject(e.target.value)}
-            />
+                <FormLabel htmlFor="subject" id="subject-label" sx={{ mt: 2 }} required>
+                    Subject
+                </FormLabel>
+                <TextField
+                    name="subject"
+                    placeholder="Subject"
+                    size="lg"
+                    variant="soft"
+                    slotProps={{
+                        input: {
+                            id: "subject",
+                            "aria-labelledby": "subject-label"
+                        }
+                    }}
+                    sx={{ width: "100%" }}
+                    onInput={(e) => {
+                        formErrors.delete("subject");
+                        setFormErrors(new Map(formErrors));
 
-            <FormLabel htmlFor="textarea" id="textarea-label" sx={{ mt: 2 }} required>
-                Describtion
-            </FormLabel>
-            <Textarea
-                minRows={5}
-                placeholder="Describe the issue you are facing"
-                size="lg"
-                variant="soft"
-                slotProps={{
-                    textarea: {
-                        id: "textarea",
-                        "aria-labelledby": "textarea-label"
-                    }
-                }}
-                required
-                onChange={(e) => setDescription(e.target.value)}
-            />
+                        const target = e.target as HTMLInputElement;
+                        target.value = target.value.slice(0, 45);
+                    }}
+                    error={formErrors.has("subject")}
+                />
 
-            <Button
-                sx={{ mt: 3, width: "30%", alignSelf: "center" }}
-                loading={loading}
-                onClick={() =>
-                    HandleReportFormSubmit({ type: selectedType, subject, description }, setLoading, navigate)
-                }
-            >
-                Submit
-            </Button>
+                <FormLabel htmlFor="textarea" id="textarea-label" sx={{ mt: 2 }} required>
+                    Description
+                </FormLabel>
+                <Textarea
+                    name="description"
+                    minRows={5}
+                    placeholder="Describe the issue you are facing"
+                    size="lg"
+                    variant="soft"
+                    slotProps={{
+                        textarea: {
+                            id: "textarea",
+                            "aria-labelledby": "textarea-label"
+                        }
+                    }}
+                    sx={{ width: "100%" }}
+                    onInput={(e) => {
+                        formErrors.delete("description");
+                        setFormErrors(new Map(formErrors));
+
+                        const target = e.target as HTMLInputElement;
+                        target.value = target.value.slice(0, 500);
+                    }}
+                    error={formErrors.has("description")}
+                />
+
+                <Button
+                    sx={{ mt: 3, width: "30%", alignSelf: "center", justifySelf: "center" }}
+                    loading={loading}
+                    type="submit"
+                >
+                    Submit
+                </Button>
+
+                {showAlert && (
+                    <Alert
+                        size="lg"
+                        key="Error"
+                        sx={{ mt: 2 }}
+                        variant="soft"
+                        color="danger"
+                        startDecorator={<Report />}
+                        endDecorator={
+                            <IconButton variant="soft" size="sm" color="danger" onClick={() => setShowAlert(false)}>
+                                <CloseRounded />
+                            </IconButton>
+                        }
+                    >
+                        {formErrors.has("courseId")
+                            ? "Invalid course. Please make sure you are reporting a valid course."
+                            : "An error occurred while submitting the report. Please try again later."}
+                    </Alert>
+                )}
+            </Sheet>
         </Sheet>
     );
 };
--- client/src/components/report/thread/ReplyForm.tsx
+++ client/src/components/report/thread/ReplyForm.tsx
@@ -1,16 +1,48 @@
-import { Box, Button, Textarea } from "@mui/joy";
+import { Report, CloseRounded } from "@mui/icons-material";
+import { Sheet, Button, Textarea, Alert, IconButton } from "@mui/joy";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import * as Yup from "yup";
 
-import { HandleThreadReplyFormSubmit } from "@internals/handlers";
+import { AddThreadReply } from "@internals/api";
+import { validateFormData } from "@internals/utils";
+
+const validationRules = {
+    reply: Yup.string().required("Reply is required").max(500, "Reply must be less than 500 characters")
+};
 
 const ReplyForm: React.FC = () => {
-    const [message, setMessage] = useState("");
+    const [formErrors, setFormErrors] = useState(new Map());
+    const [showAlert, setShowAlert] = useState(false);
     const [loading, setLoading] = useState(false);
+
     const { reportId } = useParams<{ reportId: string }>();
 
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setLoading(true);
+
+        setFormErrors(new Map());
+        setShowAlert(false);
+
+        const reply = e.currentTarget.reply.value;
+
+        validateFormData({ reply }, validationRules)
+            .then(async (data) => {
+                const validatedData = data as unknown as { reply: string };
+
+                await AddThreadReply(reportId, validatedData.reply)
+                    .then(() => window.location.reload()) // TODO: use Redux to update state instead
+                    .catch(() => setShowAlert(true))
+                    .finally(() => setLoading(false));
+            })
+            .catch((errors) => setFormErrors(errors))
+            .finally(() => setLoading(false));
+    };
+
     return (
-        <Box
+        <Sheet
             sx={{
                 width: "100%",
                 display: "flex",
@@ -20,18 +52,27 @@ const ReplyForm: React.FC = () => {
                 alignSelf: "center",
                 gap: 2
             }}
+            component="form"
+            onSubmit={handleFormSubmit}
         >
             <Textarea
+                name="reply"
                 placeholder="Type a reply..."
                 minRows={3}
                 sx={{
                     width: "100%",
                     borderRadius: "sm",
-                    boxShadow: "0 0 0 0px rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)",
+                    boxShadow: "0 2px 8px 0 rgba(0, 0, 0, 0.1)",
                     alignSelf: "center"
                 }}
                 variant="soft"
-                onChange={(e) => setMessage(e.target.value)}
+                error={formErrors.has("reply")}
+                onInput={(e) => {
+                    setFormErrors(new Map());
+
+                    const target = e.target as HTMLTextAreaElement;
+                    target.value = target.value.slice(0, 500);
+                }}
             />
             <Button
                 loading={loading}
@@ -42,11 +83,31 @@ const ReplyForm: React.FC = () => {
                 }}
                 variant="soft"
                 color="primary"
-                onClick={() => HandleThreadReplyFormSubmit(reportId, message, setLoading)}
+                type="submit"
             >
                 <b>Post</b>
             </Button>
-        </Box>
+
+            {showAlert && (
+                <Alert
+                    size="lg"
+                    key="Error"
+                    sx={{ mt: 2 }}
+                    variant="soft"
+                    color="danger"
+                    startDecorator={<Report />}
+                    endDecorator={
+                        <IconButton variant="soft" size="sm" color="danger" onClick={() => setShowAlert(false)}>
+                            <CloseRounded />
+                        </IconButton>
+                    }
+                >
+                    {formErrors.has("courseId")
+                        ? "Invalid course. Please make sure you are reporting a valid course."
+                        : "An error occurred while submitting the report. Please try again later."}
+                </Alert>
+            )}
+        </Sheet>
     );
 };
 
