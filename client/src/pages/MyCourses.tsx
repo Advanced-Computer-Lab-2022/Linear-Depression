2d186b7bc06ed57fc6e6c7f5adb38ba74cfe1dc6
AhmedNasserG <ahmednasser217217@gmail.com>
2022-12-28 23:00:44 +0200

    fix: remove add course option from trainee

--- client/src/pages/MyCourses.tsx
+++ client/src/pages/MyCourses.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { openModal } from "react-url-modal";
 
 import { CoursesListWithFilters, FloatingButton, Navbar } from "@internals/components";
-import { useFetchMyCourses, useGetInstructorContractStatus } from "@internals/hooks";
+import { useAuth, useFetchMyCourses, useGetInstructorContractStatus } from "@internals/hooks";
 import { useAppSelector } from "@internals/redux";
+import { User } from "@internals/types";
 
 const MyCourses: React.FC = () => {
     useFetchMyCourses();
     useGetInstructorContractStatus();
+    const { auth } = useAuth();
 
     const { data, loading } = useAppSelector((state) => state.coursesList);
 
@@ -22,9 +24,11 @@ const MyCourses: React.FC = () => {
         <>
             <Navbar />
             {!loading && <CoursesListWithFilters courses={data} addCourse={true} />}
-            <FloatingButton color="primary" aria-label="add" onClick={onClick}>
-                <AddIcon />
-            </FloatingButton>
+            {auth.userType === User.INSTRUCTOR && (
+                <FloatingButton color="primary" aria-label="add" onClick={onClick}>
+                    <AddIcon />
+                </FloatingButton>
+            )}
         </>
     );
 };
