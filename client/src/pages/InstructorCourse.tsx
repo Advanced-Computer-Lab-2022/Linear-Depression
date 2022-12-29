6e784dff17f87d9e14ab9be8b2eb35d214770987
ShimaaBetah <shimaabetah1911@gmail.com>
2022-12-30 00:04:12 +0200

    Add report Button and move refund button to down

--- client/src/pages/InstructorCourse.tsx
+++ client/src/pages/InstructorCourse.tsx
@@ -1,16 +1,37 @@
 import AddIcon from "@mui/icons-material/Add";
-import React from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { openModal } from "react-url-modal";
 import styled from "styled-components";
 
 import { CourseContent, CourseHeader, CourseReviews, FloatingButton } from "@internals/components";
-import { useAuth, useFetchCourseById, useFetchMyEnrollment } from "@internals/hooks";
+import { useAuth, useFetchCourseById, useFetchMyEnrollment, useFetchMyRefundRequest, useToast } from "@internals/hooks";
 import { useAppSelector } from "@internals/redux";
+import { sendRefundRequest, cancelRefundRequest } from "@internals/services";
 import { User } from "@internals/types";
 
 const Container = styled.div`
     margin: 0 30% 0 100px;
+`;
+const Button = styled(LoadingButton)`
+    width: 100%;
+    height: 48px;
+    font-weight: 700;
+    font-size: 16px;
+    margin: 5px;
+    background-color: white;
+    color: black;
+    &:hover {
+        background-color: #f5f3fe;
+        color: black;
+    }
+    border: 1px solid black;
+`;
+const HorizontalContainer = styled.div`
+    display: flex;
+    flex-direction: row;
 `;
 
 const InstructorCourse: React.FC = () => {
@@ -21,6 +42,12 @@ const InstructorCourse: React.FC = () => {
     useFetchCourseById(courseId);
     useFetchMyEnrollment(courseId);
     const { data } = useAppSelector((state) => state.course);
+    const enrollment = useAppSelector((state) => state.enrollment);
+    const { refundRequest, updateRefundRequest } = useFetchMyRefundRequest(enrollment.data?._id);
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+    const { showToast } = useToast();
 
     const onClick = () => {
         openModal({
@@ -29,6 +56,41 @@ const InstructorCourse: React.FC = () => {
                 courseId
             }
         });
+    };
+    const handleRefund = () => {
+        setLoading(true);
+        if (userType === User.INDIVIDUAL_TRAINEE) {
+            sendRefundRequest(enrollment.data?._id)
+                .then(() => {
+                    showToast({ message: "Refund request sent successfully", type: "success" });
+                    updateRefundRequest();
+                })
+                .catch((err) => {
+                    showToast({ message: "Failed to send refund request", type: "error" });
+                    console.log(err);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }
+    };
+
+    const handleCancelRefund = () => {
+        setLoading(true);
+        if (userType === User.INDIVIDUAL_TRAINEE) {
+            cancelRefundRequest(enrollment.data?._id)
+                .then(() => {
+                    showToast({ message: "Refund request cancelled", type: "success" });
+                    updateRefundRequest();
+                })
+                .catch((err) => {
+                    showToast({ message: "Failed to cancel refund request", type: "error" });
+                    console.log(err);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }
     };
 
     // TODO: To be replaced with suspense
@@ -42,6 +104,28 @@ const InstructorCourse: React.FC = () => {
             <Container>
                 <CourseContent lessons={data.lessons} />
                 <CourseReviews />
+                <HorizontalContainer>
+                    <Button
+                        onClick={() => {
+                            navigate(`/me/reports/new?course_id=${courseId}`);
+                        }}
+                    >
+                        Report An issue
+                    </Button>
+                    {userType === User.INDIVIDUAL_TRAINEE &&
+                        enrollment.data !== null &&
+                        enrollment.data?.progress < 50 &&
+                        refundRequest.data == null && (
+                            <Button loading={loading} onClick={handleRefund}>
+                                Request Refund
+                            </Button>
+                        )}
+                    {userType === User.INDIVIDUAL_TRAINEE && enrollment.data && refundRequest.data && (
+                        <Button loading={loading} onClick={handleCancelRefund}>
+                            Cancel Refund
+                        </Button>
+                    )}
+                </HorizontalContainer>
             </Container>
             {userType === User.INSTRUCTOR && !data.isPublished && (
                 <FloatingButton onClick={onClick}>
