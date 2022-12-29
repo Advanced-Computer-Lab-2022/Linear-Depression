b83f2718d0bfee9fd9d99db60f8a928595f41868
Abdulaziz Hassan <abdulazizhza@gmail.com>
2022-12-29 16:41:39 +0200

    Add subjects dropdown list when creating new course (#263)
    * Add subjects dropdown list when creating new course
    
    * Fix bug in dropdown list when entering custom subject

---
+++ client/src/components/modals/AddSubject.tsx
@@ -0,0 +1,46 @@
+import { Dialog, DialogContent, DialogContentText, DialogActions, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
+
+const AddSubject: React.FC<{
+    open: boolean;
+    onClose: (data: string) => void;
+}> = ({ open, onClose }) => {
+    const [subject, setSubject] = useState("");
+
+    const handleClose = () => {
+        onClose(null);
+    };
+
+    const handleSubmit = () => {
+        onClose(subject);
+    };
+
+    return (
+        <Dialog open={open}>
+            <DialogContent>
+                <DialogContentText>Add a new Subject</DialogContentText>
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="subject"
+                    label="Subject"
+                    type="text"
+                    fullWidth
+                    variant="outlined"
+                    value={subject}
+                    multiline
+                    minRows={1}
+                    onChange={(e) => setSubject(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleSubmit}>Submit</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default AddSubject;
