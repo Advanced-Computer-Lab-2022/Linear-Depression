2913fdcc8d2dfe3fb56f8acd1f075ca93ca8bc84
AhmedNasserG <ahmednasser217217@gmail.com>
2022-12-30 11:00:31 +0200

    Spinner when loading courses

--- client/src/components/coursesListWithFilters/CoursesList.tsx
+++ client/src/components/coursesListWithFilters/CoursesList.tsx
@@ -1,7 +1,9 @@
+import { CircularProgress } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 
 import CourseCard from "./coursesList/CourseCard";
+import { useAppSelector } from "@internals/redux";
 import { Course as ICourseProps } from "@internals/types";
 
 const CoursesListContainer = styled.div`
@@ -13,8 +15,37 @@ const CoursesList: React.FC<{ courses: ICourseProps[]; showPrice: boolean; showS
     showPrice,
     showStatus
 }) => {
+    const { loading } = useAppSelector((state) => state.coursesList);
+
+    if (loading) {
+        return (
+            <CoursesListContainer>
+                <CircularProgress
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)"
+                    }}
+                />
+            </CoursesListContainer>
+        );
+    }
+
     return (
         <CoursesListContainer>
+            {courses.length === 0 && (
+                <p
+                    style={{
+                        textAlign: "center",
+                        fontSize: "1.5rem",
+                        fontWeight: "bold",
+                        color: "var(--color-grey-dark-1)"
+                    }}
+                >
+                    No courses available right now
+                </p>
+            )}
             {courses.map((course) => (
                 <CourseCard key={course._id} course={course} showPrice={showPrice} showStatus={showStatus} />
             ))}
