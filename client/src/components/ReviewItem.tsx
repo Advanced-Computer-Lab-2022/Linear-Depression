f3214da43c962e880b91d931e952e4eb3d0202fd
Mohammad Omar <72332009+MohammadOTaha@users.noreply.github.com>
2023-01-01 13:45:07 +0200

    feat: remove reviews likes and reports (#312)

--- client/src/components/ReviewItem.tsx
+++ client/src/components/ReviewItem.tsx
@@ -1,6 +1,5 @@
 import moment from "moment";
 import React from "react";
-import { AiFillLike, AiFillDislike } from "react-icons/ai";
 import StarRatings from "react-star-ratings";
 import styled from "styled-components";
 
@@ -42,39 +41,6 @@ const Subtitle = styled.div`
     margin-bottom: 8px;
 `;
 
-const ActionsContainer = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-const ActionButton = styled.button`
-    border-radius: 50%;
-    height: 40px;
-    width: 40px;
-    border: 1px solid black;
-    margin-right: 8px;
-    background-color: transparent;
-`;
-
-const LikeIcon = styled(AiFillLike)`
-    height: 20px;
-    width: 20px;
-    margin-bottom: 2px;
-`;
-
-const DislikeIcon = styled(AiFillDislike)`
-    height: 20px;
-    width: 20px;
-    margin-bottom: 2px;
-`;
-
-const Report = styled.div`
-    font-weight: 400;
-    font-size: 14px;
-    text-decoration: underline;
-    color: black;
-`;
-
 const HorizontalLine = styled.hr`
     border: 1px solid #e6e6e6;
     margin-top: 24px;
@@ -107,16 +73,6 @@ const ReviewItem: React.FC<{
                         <Date>{moment(date).fromNow()}</Date>
                     </RatingContainer>
                     <Comment>{comment}</Comment>
-                    <Subtitle>Was this review helpful?</Subtitle>
-                    <ActionsContainer>
-                        <ActionButton>
-                            <LikeIcon />
-                        </ActionButton>
-                        <ActionButton>
-                            <DislikeIcon />
-                        </ActionButton>
-                        <Report>Report</Report>
-                    </ActionsContainer>
                 </div>
             </Containter>
             <HorizontalLine />
